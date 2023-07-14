f92eb889b482203b618f2d139d4df3df5387794b
tyut2018 <42170356+tyut2018@users.noreply.github.com>
2023-07-14 10:10:06 +0800

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -23,10 +23,3 @@ data_load_state.text('数据导入完成')
 st.subheader('城南与城北地区历年常住人口（万人）')
 data = pd.DataFrame(data)
 st.write(data)
-
-st.subheader('按照时间浏览常驻人口')
-chart_data = pd.DataFrame(data)
-chart_data = chart_data[1:12]
-st.write(chart_data)
-st.line_chart(chart_data, y=columns)
-
